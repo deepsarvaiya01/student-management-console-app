21855f22cc31f2b564a2ffdc29f11642d4c76edd
Deep Sarvaiya <deep.sarvaiya@tssconsultancy.com>
2025-07-29 11:46:26 +0530

    DB Connection by deep

--- student_management_system_app/src/com/tss/sms/dbconnection/DBConfig.java
+++ student_management_system_app/src/com/tss/sms/dbconnection/DBConfig.java
@@ -1,5 +1,14 @@
 package com.tss.sms.dbconnection;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 public class DBConfig {
+	
+    // Load environment variables from .env
+    final static Dotenv dotenv = Dotenv.load();
+    
+    static final String DB_URL = dotenv.get("DB_URL");
+    static final String DB_USERNAME = dotenv.get("DB_USERNAME");
+    static final String DB_PASSWORD = dotenv.get("DB_PASSWORD");
 
 }
